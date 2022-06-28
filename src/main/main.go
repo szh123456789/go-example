32fdc99874a921d29bc7f1a851319010573e53d4
szh <1696646098@qq.com>
2022-06-28 18:23:38 +0800

    基本变量

--- src/main/main.go
+++ src/main/main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-ole/go-ole"
 )
 
+//一般用于定义全局变量
+var (
+	a bool
+	b string
+	c int
+	d int8
+	e int16
+	f int32
+	g int64
+    h uint
+	i uint8
+	j uint16
+	k uint32
+	l uint64
+	m uintptr
+    n byte // uint8 的别名
+    o rune // int32 的别名 代表一个 Unicode 码
+    p float32
+	q float64
+    r complex64
+	s complex128
+)
 func main() {
-
-	fmt.Println(ole.IID_IUnknown)
-	fmt.Print("hello world")
+	fmt.Println(a)
+	fmt.Println(b + "string")
+	fmt.Println(c )
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
+	fmt.Println(m)
+	fmt.Println(n)
+	fmt.Println(o)
+	fmt.Println(p)
+	fmt.Println(q)
+	fmt.Println(r)
+	fmt.Println(s)
 }
